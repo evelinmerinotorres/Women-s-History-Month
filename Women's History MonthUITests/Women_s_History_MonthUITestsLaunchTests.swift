4247964093e329bdb13acdf89ffbd3f61b19de74
Evelin Merino-Torres <em303258@gmail.com>
2022-03-30 08:29:37 -0400

    Initial Commit

---
+++ Women's History MonthUITests/Women_s_History_MonthUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Women_s_History_MonthUITestsLaunchTests.swift
+//  Women's History MonthUITests
+//
+//  Created by Evelin Merino-Torres on 3/30/22.
+//
+
+import XCTest
+
+class Women_s_History_MonthUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
